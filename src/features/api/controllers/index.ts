a073f1b52913cbeee7273a315c3151c90f785912
Akash NP <akashnp1998@gmail.com>
2023-09-26 21:23:26 +0400

    feat: add mock response functionality

--- src/features/api/controllers/index.ts
+++ src/features/api/controllers/index.ts
@@ -5,10 +5,15 @@ type UserPrompt = {
     prompt: string
 }
 
+type QueryParams = {
+    mock?: string
+}
+
 export async function conversationController(
-    req: Request<{}, any, UserPrompt>,
+    req: Request<{}, any, UserPrompt, QueryParams>,
     res: Response
 ) {
+    if (req.query.mock) res.send("mock response")
     const { body } = req;
     const response = await getAiResponse(body.prompt);
     res.send(response)
